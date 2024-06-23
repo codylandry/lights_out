4005c2d24771c0d829d66f697bde0e27db511668
codylandry <cody1786@gmail.com>
2024-06-23 01:33:00 -0400

    css work

--- lib/lights_out_web/components/core_components.ex
+++ lib/lights_out_web/components/core_components.ex
@@ -202,7 +202,7 @@ defmodule LightsOutWeb.CoreComponents do
   def simple_form(assigns) do
     ~H"""
     <.form :let={f} for={@for} as={@as} {@rest}>
-      <div class="mt-10 space-y-8 bg-white">
+      <div class="mt-10 space-y-8">
         <%= render_slot(@inner_block, f) %>
         <div :for={action <- @actions} class="mt-2 flex items-center justify-between gap-6">
           <%= render_slot(action, f) %>
@@ -396,7 +396,7 @@ defmodule LightsOutWeb.CoreComponents do
 
   def label(assigns) do
     ~H"""
-    <label for={@for} class="block text-sm font-semibold leading-6 text-zinc-800">
+    <label for={@for} class="block text-sm font-semibold leading-6 text-slate-100">
       <%= render_slot(@inner_block) %>
     </label>
     """
